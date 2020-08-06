4c25f460d62a877feb7b63a3bfb625c7d7ee4910
Cory Crook <cory@akira.md>
2020-08-06 16:02:35 -0400

    Updates

---
+++ precache-manifest.3f16ba3059ff696985e3cb8807b16043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b108743619ae3c5ce99febe43ff7cd3",
+    "url": "/weekly-planner/index.html"
+  },
+  {
+    "revision": "04c06172261416ed2d60",
+    "url": "/weekly-planner/static/css/main.4fa8a350.chunk.css"
+  },
+  {
+    "revision": "564e82796c646ffb5e3f",
+    "url": "/weekly-planner/static/js/2.d00aafec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weekly-planner/static/js/2.d00aafec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c06172261416ed2d60",
+    "url": "/weekly-planner/static/js/main.d335ef58.chunk.js"
+  },
+  {
+    "revision": "11bd441119c10d72842d",
+    "url": "/weekly-planner/static/js/runtime-main.2296fae7.js"
+  }
+]);
